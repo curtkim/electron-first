fa08c1dba9a9e99bd0d0d98506bd9fc428d301c6
curt <curt.k@kakaomobility.com>
2020-11-17 03:04:42 +0900

    websocket-with-header에서 ipcRender event를 rxjs.fromEvent로 받음

--- websocket-with-header/src/main.js
+++ websocket-with-header/src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { fromEvent } from 'rxjs';
 
+
+const myipc$ = fromEvent(window.ipcRenderer, 'my-ipc')
+myipc$.subscribe(x => console.log(x))
+
+/*
 window.ipcRenderer.on('my-ipc', (event, msg) => {
   console.log(msg)
 })
+*/
 
 Vue.config.productionTip = false
 
